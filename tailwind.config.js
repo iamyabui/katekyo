74b0bd20c210898db4267beb48ee73a8119ddedd
Erika Yabui <yerika.1127@gmail.com>
2022-02-26 09:37:18 +0900

    1st push

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "subject-gray": "#D8D8D8",
+        "origin-gray": "#808080",
+        "origin-deepGray": "#727272",
+        "origin-purple": "#7835E4",
+        "origin-blue": "#00B2FF",
+        "origin-deepBlue": "#06A4E8",
+        "origin-pink": "#F36789",
+        "origin-deepPink": "#E36180",
+        "origin-green": "#74D76B",
+        "card-purple": "#EAD7F2",
+        "card-blue": "#DBF0FF",
+        "card-gray": "#EEEEEE",
+        "top-bg": "#FAFAFA",
+      },
+    },
+  },
+  plugins: [],
+};
