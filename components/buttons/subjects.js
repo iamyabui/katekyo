5a78bc50a69d61a38493dfac6add0b7d54873b04
Erika Yabui <yerika.1127@gmail.com>
2022-02-26 15:26:16 +0900

    先生用先生プロフィール詳細と編集画面の作成

--- components/buttons/subjects.js
+++ components/buttons/subjects.js
@@ -1,8 +1,10 @@
-export default function Subject() {
+export default function Subject(props) {
+  const { subject } = props;
+
   return (
     <>
-      <button className="bg-subject-gray hover:bg-origin-purple hover:text-white text-origin-purple px-1 mx-1 rounded">
-        #数学
+      <button className="bg-subject-gray hover:bg-origin-purple hover:text-white text-origin-purple px-1 my-1 mx-1 rounded">
+        {subject}
       </button>
     </>
   );
---
+++ pages/myselfTeacherDetail.js
@@ -0,0 +1,33 @@
+import TeacherLeftMenu from "../components/common/teacherLeftMenu";
+import TeacherProfileDetail from "../components/cards/teacherProfileDetail";
+import Edit from "../components/buttons/edit";
+
+export default function MyselfTeacherDetail() {
+  return (
+    <div className="bg-top-bg h-screen w-screen ">
+      <div className="flex max-w-7xl mx-auto py-10">
+        <TeacherLeftMenu />
+        <TeacherProfileDetail />
+        <div className="flex-column mx-10 px-10 w-[40rem] bg-white p-8 rounded text-gray-700">
+          <p>自己紹介</p>
+          <p>
+            桜学院の山田です。 部活はバトン部に所属していました。
+            BTSやTWICEなどのKPOPが好きです！
+          </p>
+          <p>目標</p>
+          <p>
+            早稲田大学の工学部を目指しています。
+            夏までに、過去問が解けるようになっていたいです。
+          </p>
+          <p>先生への要望</p>
+          <p>
+            物理が苦手なので、苦手克服をサポートいただけると嬉しいです。物理が苦手なので、苦手克服をサポートいただけると嬉しいです。
+          </p>
+          <div className="mt-10 flex justify-end">
+            <Edit />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
