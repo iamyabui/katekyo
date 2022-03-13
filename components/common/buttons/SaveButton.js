c118c2636528dd0af21a8f3d5efe66191ff1f921
Erika Yabui <yerika.1127@gmail.com>
2022-03-13 23:29:51 +0900

    useState情報の参照と編集機能の実装

--- components/common/buttons/SaveButton.js
+++ components/common/buttons/SaveButton.js
@@ -1,7 +1,31 @@
+import { useRecoilValue } from "recoil";
+import { userState } from "../atoms";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../src/firabase";
+
 export default function Save() {
+  const user = useRecoilValue(userState);
+
+  async function handleSave() {
+    console.log(user.id);
+    try {
+      await updateDoc(doc(db, "StudentUsers", user.id), {
+        name: user.name,
+        school: user.school,
+        grade: user.grade,
+        text: user.text,
+        goal: user.goal,
+        request: user.request,
+      });
+    } catch (error) {}
+  }
+
   return (
     <>
-      <button className="bg-origin-gray hover:bg-origin-deepGray text-white px-5 py-1 rounded">
+      <button
+        onClick={handleSave}
+        className="bg-origin-gray hover:bg-origin-deepGray text-white px-5 py-1 rounded"
+      >
         保存
       </button>
     </>
--- components/student/profileEdit/StudentProfileEditCard.js
+++ components/student/profileEdit/StudentProfileEditCard.js
@@ -3,7 +3,7 @@ import School from "./SchoolNameForm";
 import Grade from "./GradePulldown";
 import IconBig from "../../common/icon/BigIcon";
 
-export default function StudentProfileEdit() {
+export default function StudentProfileEditCard() {
   return (
     <div className="w-52 h-82 py-5 px-5 bg-card-purple rounded-md flex flex-col justify-center items-center text-gray-700">
       <p className="my-1">Profile</p>
