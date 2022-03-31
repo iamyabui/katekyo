8b5fbd90ac2aa37144ea21ea06068971f313e905
薮井えりか <Erika@MacBook-Air.local>
2022-03-31 20:38:49 +0900

    残タスクのカテゴリデフォルト選択について追加

--- components/common/pulldown/CategoryPulldown.js
+++ components/common/pulldown/CategoryPulldown.js
@@ -11,7 +11,7 @@ export default function Category() {
   return (
     <>
     <div className="inline-block relative w-32">
-      <select onChange={handleCategory} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+      <select onChange={handleCategory} value={editUser.category} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
         <option value="">選択</option>
         <option value="大学受験">大学受験</option>
         <option value="中学受験">中学受験</option>
