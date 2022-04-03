ebc49e2d953759092f04e062123a4f058174c754
薮井えりか <Erika@MacBook-Air.local>
2022-04-03 23:03:47 +0900

    生徒マイページ受講状況の実装

---
+++ components/student/attendanceStatus/CancelButton.js
@@ -0,0 +1,43 @@
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { useRecoilValue } from "recoil";
+import { db } from "../../../src/firabase";
+import { studentUserState } from "../../common/StudentAtoms";
+
+export default function Cancel(props) {
+  const { courseId, setCourses } = props;
+  const student = useRecoilValue(studentUserState)
+
+  const handleCancelApply = () => {
+    const courseRef = doc(db, "Courses", courseId, "students", student.id);
+    const studentRef = doc(db, "StudentUsers",student.id, "courses", courseId);
+
+    deleteDoc(courseRef);
+    deleteDoc(studentRef);
+
+    (async () => {
+      const coursesRef = collection(db, "Courses")
+      const coursesInfo = await getDocs(coursesRef).then((snapshot) => {
+            const coursesArray = [];
+            snapshot.docs.forEach((courseDoc) => {              
+              const courseId = courseDoc.id;
+              const course = courseDoc.data();
+              coursesArray.push({ courseId, course })
+            })
+            return coursesArray;
+      })
+      setCourses(coursesInfo);
+    })();
+
+  }
+
+  return (
+    <>
+      <button 
+      className="bg-origin-gray hover:bg-origin-deepGray text-white px-2 py-1 rounded mr-5"
+      onClick={handleCancelApply}
+      >
+        <p className="text-sm">申請をキャンセル</p>
+      </button>
+    </>
+  );
+}
--- components/student/attendanceStatus/FinishButton.js
+++ components/student/attendanceStatus/FinishButton.js
@@ -1,8 +1,43 @@
-export default function Finish() {
+import { collection, doc, getDoc, getDocs, serverTimestamp, updateDoc } from "firebase/firestore";
+import { useRecoilValue } from "recoil";
+import { db } from "../../../src/firabase";
+import { studentUserState } from "../../common/StudentAtoms";
+
+export default function Finish(props) {
+  const { course, setCourses } = props;
+  const student = useRecoilValue(studentUserState)
+
+  console.log(course.id)
+  console.log(student.id)
+
+  const handleFinish = () => {
+    const CourseRef = doc(db, "Courses", course.id, "students", student.id);
+    getDoc(CourseRef).then((snapshot) => {
+      console.log(snapshot.data());
+    })
+    // updateDoc(CourseRef, { status: "終了申請中", finish_date: serverTimestamp()})
+
+    // 生徒情報を取得
+    // (async () => {
+    //   const coursesRef = collection(db, "Courses")
+    //   const coursesInfo = await getDocs(coursesRef).then((snapshot) => {
+    //         const coursesArray = [];
+    //         snapshot.docs.forEach((courseDoc) => {              
+    //           const courseId = courseDoc.id;
+    //           const course = courseDoc.data();
+    //           coursesArray.push({ courseId, course })
+    //         })
+    //         return coursesArray;
+    //   })
+    //   setCourses(coursesInfo);
+    // })();
+  }
+
   return (
     <>
-      <button className="bg-origin-gray hover:bg-origin-deepGray text-white px-5 py-1 rounded">
-        終了する
+      <button className="bg-origin-gray hover:bg-origin-deepGray text-white px-2 py-1 rounded mr-5">
+      <p 
+      className="text-sm" onClick={handleFinish}>終了</p>
       </button>
     </>
   );
