796e005161ab3f04943043fb1193c54c7072b317
薮井えりか <Erika@MacBook-Air.local>
2022-04-06 10:03:15 +0900

    ChatListの各生徒とのやり取りについて最新メッセージ表示するよう修正

--- pages/teacherChatList.js
+++ pages/teacherChatList.js
@@ -4,28 +4,88 @@ import TeacherLeftMenu from "../components/teacher/common/TeacherLeftMenu";
 import { useRecoilValue } from "recoil";
 import { teacherUserState } from "../components/common/TeacherAtoms";
 import { useEffect, useState } from "react";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "../src/firabase";
 import Router from "next/router";
 
 export default function TeacherChatRoom() {
   const teacher = useRecoilValue(teacherUserState);
   const [contactList, setContactList] = useState([]);
+  const [chatsList, setChatsList] = useState([]);
+  const [latestMessageWithStudentInfo, setLatestMessageWithStudentInfo] = useState([]);
 
-  // 該当ログイン先生ユーザーIDのcontactサブコレクションを取得
+  // ①該当ログイン先生ユーザーIDのcontactサブコレクションを一旦取得
   useEffect(() => {
     const teacherRef = doc(db, "TeacherUsers", teacher.id);
     const contactsRef = getDocs(collection(teacherRef, "contacts"));
 
     contactsRef.then(snapshot => {
         const contacts = snapshot.docs.map((doc) => {
-            const id = doc.id
+            const studentId = doc.id;
             const name = doc.data().name;
-            return { id: id, name: name }
+            return { id: studentId, name: name }
         })
         setContactList(contacts);
     })
-}, [])
+  }, [])
+
+  // ②chatコレクションの全内容を、chatIdとフィールドをオブジェクトとして一旦取得
+  useEffect(() => {
+    const ChatsRef = getDocs(collection(db, "Chats"));
+    ChatsRef.then(snapshot => {
+      const chats = snapshot.docs.map((doc) => {
+        const chatId = doc.id;
+        const contact1 = doc.data().sender_user;
+        const contact2 = doc.data().receive_user;
+        return { chatId: chatId, contact1: contact1, contact2: contact2}
+      })
+      setChatsList(chats);
+    })
+  },[contactList])
+
+  // ①と②で取得した、contactList（連絡先に登録されている生徒たち）とchatsList（チャットのすべて）を利用して、
+  // 各生徒とログイン先生ユーザーの最新メッセージを取得。
+  useEffect(() => {
+    (async () => {
+
+      const latestMessageWithStudentInfo = await Promise.all(contactList.map(async (student) => {
+        const studentId = student.id;
+        const teacherId = teacher.id;
+        const studentName = student.name;
+
+        // chat:既にチャットリストに登録されている生徒とログイン先生ユーザーのやりとりchatを探して取得
+        const chat = chatsList.map((chat) => {
+          if((chat.contact1 == studentId && chat.contact2 == teacherId) || (chat.contact1 == teacherId && chat.contact2 == studentId)) {
+            return chat;
+          }
+        }).filter(Boolean)
+
+        const chatId = chat[0].chatId;
+
+        // 上で取得したChatIDからMessagesコレクションを、最新順にして配列で取得。
+        const MessageRef = collection(db, "Chats", chatId, "Messages");
+        const q = query(MessageRef, orderBy("time", "desc"));
+
+        const messages = await getDocs(q).then(snapshot => {
+          const messageArray = [];
+          snapshot.docs.forEach((doc) => {
+            const message = doc.data().text;
+            const time = doc.data().time;
+            messageArray.push({ message, time });
+          });
+          return messageArray;                    
+        })
+
+        // latestMessage:messageArrayから最新のMessageを取得。
+        const latestMessage = messages[0];
+        return({ studentId: studentId, studentName: studentName, latestMessage: latestMessage})
+      }));
+
+      console.log(latestMessageWithStudentInfo);
+      setLatestMessageWithStudentInfo(latestMessageWithStudentInfo);
+
+    })()
+  },[chatsList])
 
   const handleMoveToChatroom = (id) => {
     Router.push({
@@ -41,8 +101,8 @@ export default function TeacherChatRoom() {
         <div className="flex max-w-5xl mx-auto py-10 h-screen">
           <TeacherLeftMenu />
           <div>
-          {contactList.map((student, index) => (
-              <div onClick={() => handleMoveToChatroom(student.id)} key={index} className="text-sm my-2">
+          {latestMessageWithStudentInfo.map((student, index) => (
+              <div onClick={() => handleMoveToChatroom(student.studentId)} key={index} className="text-sm my-2">
               <ChatNameList student={student}/>
               </div>
           ))}
